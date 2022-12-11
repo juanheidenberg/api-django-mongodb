8b214270837f2d06e7f63eb4bb90142ee6988966
juanheidenberg <juanheidenberg@gmail.com>
2022-12-10 22:46:49 -0500

    primer commit

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'DjangoApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'CLIENT': {
+            "host": "mongodb+srv://juanheidenberg:jafranco2021@cluster0.h0iauxj.mongodb.net/?retryWrites=true&w=majority",
+            "name": "mytestdb",
+            "authMechanism": "SCRAM-SHA-1"
+        }
     }
 }
 
--- EmployeeApp/models.py
+++ EmployeeApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Department(models.Model):
-    DepartmentId = models.AutoField(primary_key=True)
+    DeparamentId = models.AutoField(primary_key=True)
     DepartmentName = models.CharField(max_length=500)
 
 class Employees(models.Model):
